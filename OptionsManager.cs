9e9ec5be7828310825e3f1abe095f9dfa7764d7e
Test-create1 <at_2007@abv.bg>
2021-03-07 10:28:18 +0200

    Division

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -24,5 +24,11 @@ namespace GitExercise
         {
             Console.WriteLine($"{a} - {b} = {a - b}");
         }
+        public static void Division(double a, double b)
+        {
+            Console.WriteLine($"{a} / {b} = {a/b}");
+        }
+
+       
     }
 }
